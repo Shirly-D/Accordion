888dcd9a3ce38513748e36127d920de89c247db8
Shirly <shirlyd@axioned.com>
2021-08-13 16:07:21 +0530

    js commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,8 @@
-var accord = document.querySelectorAll('.accord');
+var accord = document.querySelectorAll('.accord-box');
 
 for (var i = 0; i < accord.length; i++) {
     accord[i].addEventListener('click', function(){
         this.classList.toggle('active');
-        var content = this,nextElementSibling;
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        }else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        }
     })
 }
 
