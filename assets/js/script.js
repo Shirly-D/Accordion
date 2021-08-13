738c74fd0e9bc1d8e59959e31ec8d3b1d00bbb37
Shirly <shirlyd@axioned.com>
2021-08-13 15:08:00 +0530

    js initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
-/* Author: 
+var accord = document.querySelectorAll('.accord');
 
-*/
+for (var i = 0; i < accord.length; i++) {
+    accord[i].addEventListener('click', function(){
+        this.classList.toggle('active');
+        var content = this,nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        }else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    })
+}
 
 
 
